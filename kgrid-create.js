ba46f1367b9d1d035f7cb723202a203c12ad5927
George Meng <gqmeng@umich.edu>
2018-05-03 17:10:22 -0400

    Changed the default for object name; clean up

--- kgrid-create.js
+++ kgrid-create.js
@@ -11,32 +11,32 @@ const minimist = require('minimist')
 const moment = require('moment')
 program
   .name('kgrid create')
-  .description('This will initialize the knowledge object based on the specified template. \n\n  If object-name is omitted, the object will have the same name as project-name.\n\n  Use kgrid list -t to find the available templates. \n\n  Example:\n\n        kgrid create jslegacy myproject 99999-trial\n\n        cd myproject \n\n        kgrid install')
+  .description('This will initialize the knowledge object based on the specified template. \n\n  If object-name is omitted, the object will have the same name as project-name.\n\n  Use kgrid list -t to find the available templates. \n\n  Example:\n\n        kgrid create\n\n           or\n\n        kgrid create -a jslegacy myproject 99999-trial\n\n')
   .usage('<template-name> <project-name> [object-name]')
+  .option('-a, --auto','An optional mode to enter the set-up information using the template defaults.')
   .option('-i, --input','An optional mode to enter the set-up information interactively.')
-  .option('-a, --auto','An optional mode to enter the set-up information interactively.')
-
+  // .option('-o, --objonly','An option to create the knowledge object ')
  	.parse(process.argv)
 
 var template = ''
 var localtemplatedir=''
 var project = 'newproject'
-var object = 'newko'
+var object = '99999-newko'
 var version = 'v0.0.1'
 var owners =''
 var contributors =''
 var description=''
 var title=''
 const choices = [
-  'jslegacy'
-  ,'pythonlegacy'
-  ,'kotemplate'
+    'jslegacy'
+  , 'pythonlegacy'
+  , 'kotemplate'
 ]
 var tmp = 'tmp'
 var srccontainer= 'src'
 var dest = project.replace(/[\/:]/g, '-')
 var gittemplate='kgrid/ko-templates'
-var src=path.join(tmp,template)
+var src=path.join(tmp, template)
 var prop = {}
 
 var argv=minimist(process.argv.slice(2))
@@ -114,7 +114,7 @@ if (program.args.length<2 && program.auto) {
         type: 'input',
         name: 'object',
         message: 'Object Name: ',
-        default:function(a){ return a.project}
+        default:object
       },
       {
         type: 'input',

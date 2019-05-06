c86420951b67074d59ff36159c981eb815b79625
George Meng <gqmeng.meng@gmail.com>
2019-05-06 13:02:17 -0400

    Changed create.js to use 'ko' as a required arg instead of an optional flag with prompting if not supplied

--- src/commands/create.js
+++ src/commands/create.js
@@ -8,8 +8,8 @@ var topMeta = JSON.parse(JSON.stringify(kometaObj))
 
 class CreateCommand extends Command {
   async run() {
-    const {flags} = this.parse(CreateCommand)
-    let ko = flags.ko
+    const {args, flags} = this.parse(CreateCommand)
+    let ko = args.ko
     let version = flags.version || ''
     let ready = false
     topMeta.hasImplementation = []
@@ -18,58 +18,23 @@ class CreateCommand extends Command {
       if (fs.pathExistsSync(ko)) {
         this.log('Path existing. Please start over with a different name for the knowledge object.')
       } else {
-        // let responses = await inquirer.prompt([
-        //   {
-        //     type: 'input',
-        //     name: 'title',
-        //     message: 'Knowledge Object Title: ',
-        //     default: title,
-        //   },
-        // ])
-        // title = responses.title
         ready = true
       }
+      if(ready){
+        this.log('==== Create the Knowledge Object ==== ')
+        fs.ensureDirSync(ko)
+
+        // Generate Top Level Metadata
+        topMeta.title = title
+        fs.writeJsonSync(ko+'/metadata.json', topMeta, {spaces: 4})
+
+        process.chdir(ko)
+        this.log('==== Initialize the first implementation ==== ')
+        await addImplementation(version)
+        this.log('Done.')
+      }
     } else {
-      let responses = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'ko',
-          message: 'Folder name for your knowledge object: ',
-          default: 'newko',
-          validate: function (input) {
-            var done = this.async()
-            setTimeout(function () {
-              if (fs.pathExistsSync(input)) {
-                done('Path existing. You need to provide a different name for your knowledge object.')
-                return
-              }
-              done(null, true)
-            }, 500)
-          },
-        },
-        // {
-        //   type: 'input',
-        //   name: 'title',
-        //   message: 'Knowledge Object Title: ',
-        //   default: title,
-        // },
-      ])
-      ko = responses.ko
-      // title = responses.title
-      ready = true
-    }
-    if(ready){
-      this.log('==== Create the Knowledge Object ==== ')
-      fs.ensureDirSync(ko)
-
-      // Generate Top Level Metadata
-      topMeta.title = title
-      fs.writeJsonSync(ko+'/metadata.json', topMeta, {spaces: 4})
-
-      process.chdir(ko)
-      this.log('==== Initialize the first implementation ==== ')
-      await addImplementation(version)
-      this.log('Done.')
+      this.log('Please provide a name for your knowledge object. \nUsage: \n    kgrid create <ko>')
     }
   }
 }
@@ -77,8 +42,11 @@ class CreateCommand extends Command {
 CreateCommand.description = 'Create the knowledge object'
 
 CreateCommand.flags = {
-  ko: flags.string({char: 'k'}),
   version: flags.string({char: 'v'}),
 }
+
+CreateCommand.args = [
+  {name:'ko'},
+]
 
 module.exports = CreateCommand

c31ebd5ef2f5ccf2944b3e4f45414b92778521c2
gregfarris <farrisg@umich.edu>
2019-05-07 13:55:21 -0400

    remove lint testing for now, will add story to add later.  Updated setup test expected

--- test/commands/setup.test.js
+++ test/commands/setup.test.js
@@ -1,11 +1,12 @@
-const {expect, test} = require('@oclif/test')
+const {expect, test} = require('@oclif/test');
+const path = require('path');
 
 describe('test setup sets environment variable ', () => {
-  test
-  .stdout()
-  .command(['setup'])
+  test.stdout().command(['setup'])
   .it('runs setup', ctx => {
-    expect(process.env.KGRID_HOME).include('/.kgrid')
+
+    expect(process.env.KGRID_HOME).equal(path.join(process.env.HOME, '.kgrid'));
+
   })
 
 })

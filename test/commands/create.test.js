e6925b82a38a4e41520c0720ad680b38a1efb7a8
gregfarris <farrisg@umich.edu>
2019-05-09 14:03:16 -0400

    [#164911016] added a few more not happy day tests to create command

--- test/commands/create.test.js
+++ test/commands/create.test.js
@@ -1,48 +1,84 @@
 const {expect, test} = require('@oclif/test');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const shell = require('shelljs');
 const tmp = require('tmp');
 
-var tmpobj = tmp.dirSync();
+const testDirectory = shell.tempdir();
 
-shell.cd(tmpobj.name);
+//Get back to temp test directory before each test
+beforeEach(function () {
+  shell.cd(testDirectory);
+});
 
-describe('test create ', () => {
-  test
-  .stdout()
-  .command(['create' ])
-  .it('runs create with no ko name', output => {
-    expect(output.stdout).to.include('Please provide a name for your knowledge object')
-  })
+describe('test happy day create ', () => {
 
   test
   .stdout()
   .command(['create', 'test-ko', '-v', 'koversion'])
-  .it('runs create with test-ko name', output => {
+  .it('runs create with test-ko name and koversion implementation', output => {
 
     expect(output.stdout).to.include('Done');
 
-    expect( fs.existsSync( path.join(
-      tmpobj.name, "test-ko", "metadata.json") )).to.be.true
+    expect(fs.existsSync(path.join(
+      testDirectory, "test-ko", "metadata.json"))).to.be.true;
 
-    expect( fs.existsSync( path.join(
-      tmpobj.name, "test-ko", "koversion", "metadata.json")), "find metadata.json file").to.be.true;
+    expect(fs.existsSync(path.join(
+      testDirectory, "test-ko", "koversion", "metadata.json")),
+      "find metadata.json file").to.be.true;
 
-    expect( fs.existsSync( path.join(
-      tmpobj.name, "test-ko", "koversion", "package.json")), "find package.json file").to.be.true;
+    expect(fs.existsSync(path.join(
+      testDirectory, "test-ko", "koversion", "package.json")),
+      "find package.json file").to.be.true;
 
-    expect( fs.existsSync( path.join(
-      tmpobj.name, "test-ko", "koversion", "service.yaml")), "find service.yaml file").to.be.true;
+    expect(fs.existsSync(path.join(
+      testDirectory, "test-ko", "koversion", "service.yaml")),
+      "find service.yaml file").to.be.true;
 
-    expect( fs.existsSync( path.join(
-      tmpobj.name, "test-ko", "koversion", "src",  "index.js")), "find index.js file").to.be.true;
+    expect(fs.existsSync(path.join(
+      testDirectory, "test-ko", "koversion", "src", "index.js")),
+      "find index.js file").to.be.true;
 
-    expect( fs.existsSync( path.join(
-      tmpobj.name, "test-ko", "koversion", "test",  "welcome.test.js")), "find welcome.test.js file").to.be.true;
+    expect(fs.existsSync(path.join(
+      testDirectory, "test-ko", "koversion", "test", "welcome.test.js")),
+      "find welcome.test.js file").to.be.true;
 
+  });
 
-  })
+});
+describe('test not happy day create ', () => {
 
-})
+  test
+  .stdout()
+  .command(['create'])
+  .it('runs create with no ko name', output => {
+    expect(output.stdout).to.include(
+      'Please provide a name for your knowledge object')
+  });
+
+  test
+  .stdout()
+  .command(['create', 'test-ko', '-v', 'koversion'])
+  .it('runs create with existing ko name and implementation', output => {
+
+    expect(output.stdout).to.include(
+      'Path existing. Please start over with a different name for the knowledge object.');
+
+  });
+
+  test
+  .stdout()
+  .command(['create', 'test-ko', '-v', 'anotherversion'])
+  .it('runs create with existing ko name and new implementation', output => {
+
+    expect(output.stdout).to.include(
+      'Path existing. Please start over with a different name for the knowledge object.');
+
+  });
+
+});
+
+after(function () {
+  fs.remove(testDirectory);
+});
 

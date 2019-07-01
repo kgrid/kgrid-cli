1136c00b14237c486e7269ec236ff85c394a331b
George Meng <gqmeng.meng@gmail.com>
2019-07-01 15:48:56 -0400

    [#167019005] Updated play.js to use ark id for KO reference.

--- src/commands/play.js
+++ src/commands/play.js
@@ -5,101 +5,100 @@ const path= require('path')
 const fs = require('fs-extra')
 const axios = require('axios')
 const shelljs = require('shelljs')
-const kometaObj = require('../template/kometadata.json')
-const createImplementation = require('../create_implementation')
 const checkPathKoioType = require('../check_pathkoiotype')
 const documentations = require('../json/extradoc.json')
-
-var topMeta = JSON.parse(JSON.stringify(kometaObj))
+const userConfig = require('../user_config')
 
 class PlayCommand extends Command {
   async run() {
     try{
-    const {args, flags} = this.parse(PlayCommand)
-    let cust_port =flags.port
-    let targeturl='https://editor.swagger.io/'
-    let ko = args.ko
-    let srcImplementation = flags.implementation
-    let pathtype = checkPathKoioType()
-    let shelfpath = pathtype.shelfpath
-    let kopath = pathtype.kopath
-    let implpath = pathtype.implpath
-
-    let topMeta = {}
-    let koid = {naan:'',name:'',imp:''}
-    if(pathtype.type=='shelf'){
-      if(ko){
-        kopath=path.join(pathtype.shelfpath,ko)
-        if(srcImplementation){
-          implpath = path.join(kopath, srcImplementation)
+      const {args, flags} = this.parse(PlayCommand)
+      let cust_port =flags.port
+      let targeturl='https://editor.swagger.io/'
+      let ark = args.ark
+      let pathtype = checkPathKoioType()
+      let shelfpath = pathtype.shelfpath
+      let kopath = pathtype.kopath
+      let implpath = pathtype.implpath
+      let metaJSON = {}
+      let koid = {naan:'',name:'',imp:''}
+      let arkid= []
+      if(ark) {
+        arkid = ark.split('/')
+        if(arkid[0]==''){
+          arkid.shift()
+        }
+        if(arkid.length<=1){
+          console.log('Please provide a valid ark id for the implementation.\n')
+          console.log('  Example: kgrid play hello/world/v1')
+          return 1
         }
       }
-    } else {
-      if(pathtype.type=='ko'){
-        if(ko){
-          if(path.join(shelfpath,ko)!=kopath){
-            console.log('Current directory is the knowledge object '+path.basename(kopath)+'.\n\nThe command line input of '+ko+' will be ignored.\n')
-          }
-        }
-        if(srcImplementation){
-          implpath = path.join(kopath, srcImplementation)
+      if(pathtype.type=='shelf'){
+        if(ark){
+          koid.naan=arkid[0] || ''
+          koid.name=arkid[1] || ''
+          koid.imp=arkid[2] || ''
         }
       } else {
-        if(pathtype.type=='implementation'){
-          if(ko){
-            if(srcImplementation){
-              if(path.join(shelfpath,ko,srcImplementation)!=implpath){
-                console.log('Current directory is the implementation '+path.basename(implpath)+' of the knowledge object '+path.basename(kopath)+'.\n\nThe command line input of '+ko+' and '+srcImplementation+' will be ignored.\n')
-              }
+        if(pathtype.type=='ko'){
+          let koMetadataPath = path.join(kopath,'metadata.json');
+          if (fs.pathExistsSync(koMetadataPath)) {
+            metaJSON = fs.readJsonSync(koMetadataPath);
+            koid.naan=metaJSON['@id'].split('-')[0]
+            koid.name=metaJSON['@id'].split('-')[1]
+          } else {
+            this.log("Cannot find metadata.json for " +path.basename(kopath));
+            return 1; // Error
+          }
+          if(ark){
+            if(koid.naan==arkid[0]&&koid.name==arkid[1]){
+              koid.imp=arkid[2] || ''
+            }else {
+              console.log('Current directory is the knowledge object of '+koid.naan+'/'+koid.name+'.\n\nThe command line input of '+ark+' will be ignored.\n')
+            }
+          }
+        } else {
+          if(pathtype.type=='implementation'){
+            let impMetadataPath = path.join(implpath,'metadata.json');
+            if (fs.pathExistsSync(impMetadataPath)) {
+              metaJSON = fs.readJsonSync(impMetadataPath);
+              koid.naan=metaJSON.identifier.split('/')[1]
+              koid.name=metaJSON.identifier.split('/')[2]
+              koid.imp=metaJSON.identifier.split('/')[3]
             } else {
-              if(path.join(shelfpath,ko)!=kopath){
-                console.log('Current directory is the implementation '+path.basename(implpath)+' of the knowledge object '+path.basename(kopath)+'.\n\nThe command line input of '+ko+' will be ignored.\n')
+              this.log("Cannot find metadata.json for " +path.basename(implpath));
+              return 1; // Error
+            }
+            if(ark){
+              if(koid.naan==arkid[0]&&koid.name==arkid[1]&&koid.imp==arkid[2]){
+
+              }else {
+                console.log('Current directory is the knowledge object of '+koid.naan+'/'+koid.name+'/'+koid.imp+'.\n\nThe command line input of '+ark+' will be ignored.\n')
               }
             }
           }
         }
       }
-    }
-
-    if(kopath!='') {
-      let koMetadataPath = path.join(kopath,'metadata.json');
-      let arkId='';
-      if (fs.pathExistsSync(koMetadataPath)) {
-        topMeta = fs.readJsonSync(koMetadataPath);
-        arkId = topMeta['@id']
-        koid.naan=arkId.split('-')[0]
-        koid.name=arkId.split('-')[1]
-      } else {
-        this.log("Cannot find metadata.json for " +path.basename(kopath));
-        return 1; // Error
+      // determine the url for the running activator
+      const userConfigJson =  userConfig()
+      let activator_port = 8080
+      if(userConfigJson){
+        if(userConfigJson.devDefault.activator_port!=''){
+          activator_port  = userConfigJson.devDefault.activator_port
+        }
       }
-    }
-    if(implpath!=''){
-      koid.imp=path.basename(implpath)
-    }
-    // determine the url for the running activator
-    let userHome = process.env.HOME || process.env.USERPROFILE || process.env.HOMEPATH ;
-    let configPath = path.join(userHome,'.config')
-    let userConfigFile = path.join(configPath, 'kgrid-cli-config.json')
-    let userConfig
-    let activator_port = 8080
-    if(fs.pathExistsSync(userConfigFile)){
-      userConfig = fs.readJsonSync(userConfigFile)
-      if(userConfig.devDefault.activator_port!=''){
-        activator_port=userConfig.devDefault.activator_port
+      if(cust_port){
+        activator_port=cust_port
       }
-    }
-    if(cust_port){
-      activator_port=cust_port
-    }
-    let url = 'http://localhost:'+activator_port+'/'
-    // Retrieve the implementation list for the KOs on the activator shelf
-    let imples = []
-    let targetimple =''
-    axios({
-      method: 'get',
-      url: url
-    })
+      let url = 'http://localhost:'+activator_port+'/kos/'
+      // Retrieve the implementation list for the KOs on the activator shelf
+      let imples = []
+      let targetimple =''
+      axios({
+        method: 'get',
+        url: url
+      })
       .then(async function (response) {
         Object.keys(response.data).forEach(function(e){
           let kometa = response.data[e]
@@ -134,7 +133,7 @@ class PlayCommand extends Command {
           } else {
             targetimple= koid.naan+'/'+koid.name+'/'+koid.imp
           }
-          targeturl = `https://editor.swagger.io/?url=${url}${targetimple}/service.yaml`
+          targeturl = `https://editor.swagger.io/?url=${url}${targetimple}/service`
           console.log(targeturl)
           console.log('will be opened in your default browser.')
           if(os.platform()=='win32'){
@@ -144,17 +143,13 @@ class PlayCommand extends Command {
           }
           return 0
         } else {
-          console.log('No implementation has been activated.')
+          console.log('No implementation with ark id of '+ koid.naan+'/'+koid.name+'/'+koid.imp+' has been activated.')
         }
-
       })
       .catch(function(error){
         if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
           console.log(error)
         } else {
-          // Something happened in setting up the request that triggered an Error
           console.log('Cannot connect to the activator at: '+url+'\n\nPlease make sure the activator is running and the correct port is specified to connect.\n\nUSAGE:\n    $ kgrid play -p [port]');
         }
       });
@@ -164,19 +159,17 @@ class PlayCommand extends Command {
   }
 }
 
-PlayCommand.description = `Create Knowledge Object and initialize the implementation.
+PlayCommand.description = `Try out a Knowledge Object implementation using Swagger Editor.
 ${documentations.play}
 `
 
 PlayCommand.flags = {
   port: flags.string({char: 'p', description:'Specify the port for KGRID Activator'}),
-  implementation: flags.string({char: 'i', description:"the name for the implementation"}),
   help: flags.help({char:'h'})
-  // , flat: flags.boolean({})
 }
 
 PlayCommand.args = [
-  {name:'ko'},
+  {name:'ark'}
 ]
 
 module.exports = PlayCommand

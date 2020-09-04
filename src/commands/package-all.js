95c50f618da039687a5d8ba78840a580c0f1237d
mradamgbeck <mradamgbeck@gmail.com>
2020-09-04 10:39:55 -0400

    minor refactors to packageall

--- src/commands/package-all.js
+++ src/commands/package-all.js
@@ -1,12 +1,11 @@
 const {Command, flags} = require('@oclif/command');
-const inquirer = require('inquirer');
-const packageKO = require('../package_ko');
+const packageKo = require('../package_ko');
 const documentations = require('../json/extradoc.json');
 const getall = require('../getall');
 
-class PackageallCommand extends Command {
+class PackageAllCommand extends Command {
   async run() {
-    const {args, flags} = this.parse(PackageallCommand);
+    const {flags} = this.parse(PackageAllCommand);
     let sourceDir = flags.source || process.cwd();
     let destDir = flags.destination || process.cwd();
     let verbose = flags.verbose;
@@ -16,13 +15,13 @@ class PackageallCommand extends Command {
     this.packageAllKO(kos, sourceDir, destDir, verbose);
   }
 
-packageAllKO(kos, sourceDir, destDir, verbose) {
+  packageAllKO(kos, sourceDir, destDir, verbose) {
 
-  kos.forEach((koMetadata) => {
-    try {
-      let koPath = sourceDir + koMetadata.path;
-      console.log("Packaging " + koMetadata.id + "-" + koMetadata.version + " at path " + koPath);
-      packageKO(koPath, destDir || koPath.substring(0, koPath.lastIndexOf(path.delimiter)), verbose);
+    kos.forEach((koMetadata) => {
+      try {
+        let koPath = sourceDir + koMetadata.path;
+        console.log("Packaging " + koMetadata.id + "-" + koMetadata.version + " at path " + koPath);
+        packageKo(koPath, destDir || koPath.substring(0, koPath.lastIndexOf(path.delimiter)), verbose);
 
       } catch (e) {
         console.log("Couldn't package " + koMetadata.id + "-" + koMetadata.version + " ");
@@ -31,15 +30,15 @@ packageAllKO(kos, sourceDir, destDir, verbose) {
   }
 }
 
-PackageallCommand.description = `Package all the knowledge objects in the specified directory.
+PackageAllCommand.description = `Package all the knowledge objects in the specified directory.
 ${documentations.packageall}
 `;
 
-PackageallCommand.flags = {
-  help: flags.help({char:'h'}),
-  source: flags.string({char:'s', description:'The folder holding the kos as the source directory'}),
-  destination: flags.string({char:'d', description:"The directory for the packaged files"}),
-  verbose: flags.boolean({char:'v', description:"Display the packaged files while added to the package"})
+PackageAllCommand.flags = {
+  help: flags.help({char: 'h'}),
+  source: flags.string({char: 's', description: 'The folder holding the kos as the source directory'}),
+  destination: flags.string({char: 'd', description: "The directory for the packaged files"}),
+  verbose: flags.boolean({char: 'v', description: "Display the packaged files while added to the package"})
 };
 
-module.exports = PackageallCommand;
+module.exports = PackageAllCommand;

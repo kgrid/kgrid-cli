6bdb3dd6af5b5211291e2a70e3dc8321381908f5
mradamgbeck <mradamgbeck@gmail.com>
2020-09-04 10:29:50 -0400

    updated templates to new @id with slashes and default version set to 1.0.0

--- src/add_kocontent.js
+++ src/add_kocontent.js
@@ -1,54 +1,51 @@
 const fs = require('fs-extra')
 const yaml = require('js-yaml')
-const path =require('path')
-const jp = require('jsonpath')
+const path = require('path')
 const source = require.resolve('../src/template/kometadata.json')
+const DEFAULT_VERSION = '1.0.0';
 
-async function addKOContent (fullpath, koid, template, runtime) {
-  let shelf = path.dirname(fullpath)
-  var sourcePath = path.join(path.dirname(source), template)
-  var topMeta = fs.readJsonSync(path.join(fullpath,'metadata.json'))
-  const serviceObj = fs.readJsonSync(path.join(sourcePath,'service.json'))
-  const deploymentObj = fs.readJsonSync(path.join(sourcePath,'deployment.json'))
-  const packageObj = fs.readJsonSync(path.join(sourcePath,'package.json'))
-  var koService = JSON.parse(JSON.stringify(serviceObj))
-  var koDeployment = JSON.parse(JSON.stringify(deploymentObj))
-  var pkgJson = JSON.parse(JSON.stringify(packageObj))
-  let artifact="src/index.js"
-  if(template=='bundlejs'&&runtime!='NodeJS'){
-    artifact="dist/main.js"
+async function addKOContent(fullpath, koid, template, runtime) {
+  let sourcePath = path.join(path.dirname(source), template);
+  let topMeta = fs.readJsonSync(path.join(fullpath, 'metadata.json'));
+  const serviceObj = fs.readJsonSync(path.join(sourcePath, 'service.json'))
+  const deploymentObj = fs.readJsonSync(path.join(sourcePath, 'deployment.json'))
+  const packageObj = fs.readJsonSync(path.join(sourcePath, 'package.json'))
+  let koService = JSON.parse(JSON.stringify(serviceObj));
+  let koDeployment = JSON.parse(JSON.stringify(deploymentObj));
+  let pkgJson = JSON.parse(JSON.stringify(packageObj));
+  let artifact = "src/index.js"
+
+  if (template === 'bundlejs' && runtime !== 'NodeJS') {
+    artifact = "dist/main.js"
   }
   topMeta.hasPayload.push(artifact)
-  fs.writeJsonSync(path.join(fullpath,'metadata.json'), topMeta, {spaces: 4})
-  // Update Service Specification
-  let artifacts = jp.apply(koService,'$..artifact',function(value){
-    return artifact
-  })
-  koService.servers[0].url = '/' + koid.naan + '/' + koid.name
-  fs.writeFileSync(path.join(fullpath,'service.yaml'),
+  fs.writeJsonSync(path.join(fullpath, 'metadata.json'), topMeta, {spaces: 4})
+
+  koService.servers[0].url = '/' + koid.naan + '/' + koid.name + '/' + DEFAULT_VERSION;
+  fs.writeFileSync(path.join(fullpath, 'service.yaml'),
     yaml.safeDump(koService, {
-      styles: { '!!null': 'canonical',}, // dump null as ~
+      styles: {'!!null': 'canonical',}, // dump null as ~
       sortKeys: false,                   // sort object keys
     })
   )
-  fs.writeFileSync(path.join(fullpath,'deployment.yaml'),
+  fs.writeFileSync(path.join(fullpath, 'deployment.yaml'),
     yaml.safeDump(koDeployment, {
-      styles: { '!!null': 'canonical',}, // dump null as ~
+      styles: {'!!null': 'canonical',}, // dump null as ~
       sortKeys: false,                   // sort object keys
     })
   )
-  pkgJson.name='@kgrid/'+ koid.naan + '-' + koid.name
-  fs.writeJsonSync(path.join(fullpath,'package.json'), pkgJson, {spaces: 4})   // Update package.JSON
+  pkgJson.name = '@kgrid/' + koid.naan + '-' + koid.name
+  fs.writeJsonSync(path.join(fullpath, 'package.json'), pkgJson, {spaces: 4})   // Update package.JSON
   fs.ensureDirSync(path.join(fullpath, 'src'))    // Create src folder for js files
-  fs.copySync(path.join(sourcePath,'src'), path.join(fullpath, 'src'))
+  fs.copySync(path.join(sourcePath, 'src'), path.join(fullpath, 'src'))
   fs.ensureDirSync(path.join(fullpath, 'test'))    // Create test folder for js files
-  fs.copySync(path.join(sourcePath,'test'), path.join(fullpath, 'test'))
+  fs.copySync(path.join(sourcePath, 'test'), path.join(fullpath, 'test'))
   // Add webpack.config.js for bundled KO
-    if(template=='bundlejs'){
-      fs.copySync(path.join(sourcePath,'webpack.config.v8'), path.join(fullpath,'webpack.config.js'))
-    }
+  if (template === 'bundlejs') {
+    fs.copySync(path.join(sourcePath, 'webpack.config.v8'), path.join(fullpath, 'webpack.config.js'))
+  }
 
   return koid.naan + '/' + koid.name
 }
 
-module.exports=addKOContent
+module.exports = addKOContent
--- src/commands/create.js
+++ src/commands/create.js
@@ -1,89 +1,92 @@
 const {Command, flags} = require('@oclif/command')
 const inquirer = require('inquirer')
-const path= require('path')
+const path = require('path')
 const fs = require('fs-extra')
-const kometaObj = require('../template/kometadata.json')
+const metadataTemplate = require('../template/kometadata.json')
+let topMeta = JSON.parse(JSON.stringify(metadataTemplate));
 const addKOContent = require('../add_kocontent')
 const documentations = require('../json/extradoc.json')
 const os = require('os')
 const userConfig = require('../user_config')
 const parseInput = require('../parse_input')
+const DEFAULT_VERSION = "1.0.0";
 
 class CreateCommand extends Command {
   async run() {
-    const {args, flags} = this.parse(CreateCommand)
+    const {args} = this.parse(CreateCommand)
     let template = 'simplejs'
     let runtime = 'Nashorn'
-    let inputPath = { naan:'', name : args.ko || '', version : ''}
-    const userConfigJson =  userConfig()
-    if(inputPath.naan ==''){
-      inputPath.naan  = os.userInfo().username
-      if(userConfigJson){
-        inputPath.naan  = userConfigJson.devDefault.naan
-      }
+    let inputPath = {naan: '', name: args.ko || '', version: ''}
+
+    const userConfigJson = userConfig()
+    if (userConfigJson) {
+      inputPath.naan = userConfigJson.devDefault.naan
+    } else {
+      inputPath.naan = os.userInfo().username
+
     }
-    if(args.ko){
-      if( args.ko.includes('-') | args.ko.includes('/') ){
+    if (args.ko) {
+      if (args.ko.includes('-') || args.ko.includes('/')) {
         console.log('Please provide a valid name for your knowledge object. \n\nAlphanumeric characters only.')
         return 1
       }
-      var parsedInput = await parseInput ('create', null, null, null, inputPath)
-      // console.log(parsedInput)
-      if(parsedInput==1){
+      let parsedInput = await parseInput('create', null, null, null, inputPath);
+
+      if (parsedInput === 1) {
+        console.log("Could not parse user input.");
         return 1
       }
-      var topMeta = JSON.parse(JSON.stringify(kometaObj))
-      let responses
-      responses = await inquirer.prompt([
+
+      let responses = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'selectedRuntime',
+          message: 'Please select the target runtime: ',
+          default: 0,
+          scroll: false,
+          choices: ['' +
+          'V8', 'NodeJS']
+        }
+      ])
+      runtime = responses.selectedRuntime;
+
+      if (runtime === 'V8') {
+        responses = await inquirer.prompt([
           {
             type: 'list',
-            name: 'selectedRuntime',
-            message: 'Please select the target runtime: ',
+            name: 'selectedTemplate',
+            message: 'Please select the template type: ',
             default: 0,
             scroll: false,
-            choices: ['Nashorn','NodeJS']
+            choices: ['Simple', 'Bundled', 'Executive']
           }
         ])
-      runtime = responses.selectedRuntime
-      console.log()
-      if(runtime=='Nashorn' | runtime=='GraalVM'){
-        responses = await inquirer.prompt([
-            {
-              type: 'list',
-              name: 'selectedTemplate',
-              message: 'Please select the template type: ',
-              default: 0,
-              scroll: false,
-              choices: ['Simple','Bundled','Executive']
-            }
-          ])
-          switch(responses.selectedTemplate){
-            case 'Simple':
-              template = 'simplejs'
-              break
-            case 'Bundled':
-              template = 'bundlejs'
-              break
-            case 'Executive':
-              template = 'executive'
-              break
-          }
-          console.log()
+        switch (responses.selectedTemplate) {
+          case 'Simple':
+            template = 'simplejs'
+            break
+          case 'Bundled':
+            template = 'bundlejs'
+            break
+          case 'Executive':
+            template = 'executive'
+            break
+        }
       } else {
         template = 'simpleproxy'
       }
       process.stdout.write('Creating the Knowledge Object ...\r')
       fs.ensureDirSync(parsedInput.fullpath)
-      topMeta.identifier = 'ark:/' + parsedInput.koid.naan + '/' + parsedInput.koid.name
-      topMeta['@id'] = parsedInput.koid.naan  + '-' + parsedInput.koid.name
-      fs.writeJsonSync(path.join(parsedInput.fullpath,'metadata.json'), topMeta, {spaces: 4})
+      topMeta.identifier = 'ark:/' + parsedInput.koid.naan + '/' + parsedInput.koid.name + '/' + DEFAULT_VERSION
+      topMeta['@id'] = parsedInput.koid.naan + '/' + parsedInput.koid.name + '/' + DEFAULT_VERSION
+      fs.writeJsonSync(path.join(parsedInput.fullpath, 'metadata.json'), topMeta, {spaces: 4})
 
       var arkid = await addKOContent(parsedInput.fullpath, parsedInput.koid, template, runtime)
-      process.stdout.write('The knowledge object '+ arkid+' has been created.\n')
-      if(template=='bundlejs'&&runtime!='NodeJS'){
-        console.log('\nPlease go to the folder by `cd '+args.ko+ '`.\n\nRun `npm install` and `npm run build` before deploying to the activator.')
-      }else {
-        console.log('\nPlease go to the folder by `cd '+args.ko+ '`.\n\nRun `npm install` before deploying to the activator.')
+      process.stdout.write('The knowledge object ' + arkid + ' has been created.\n')
+      if (template === 'bundlejs' && runtime !== 'NodeJS') {
+        console.log('\nPlease go to the folder by `cd ' + args.ko + '`.\n\nRun `npm install` and `npm run build` before deploying to the activator.')
+      } else {
+        console.log('\nPlease go to the folder by `cd ' + args.ko + '`.\n\nRun `npm install` before deploying to the activator.')
       }
     } else {
       console.log('Please provide a valid name for the Knowledge Object\n\n  Example: kgrid create myko')
@@ -96,9 +99,9 @@ CreateCommand.description = `Create Knowledge Object.
 ${documentations.create}
 `
 CreateCommand.flags = {
-  help: flags.help({char:'h'})
+  help: flags.help({char: 'h'})
 }
 CreateCommand.args = [
-  {name:'ko'}
+  {name: 'ko'}
 ]
 module.exports = CreateCommand

a682fa64e884ccd34575b4dbe5d30d3c5c76050f
Adam G Beck <mradamgbeck@gmail.com>
2021-10-01 15:44:48 -0400

    Download test (#10)
    * tested download with a single local file
    
    * added tests for manifest flag in download command
    
    * tested that the temp directory is deleted

---
+++ test/unit/commands/download.test.js
@@ -0,0 +1,165 @@
+const {expect, test} = require('@oclif/test');
+const td = require('testdouble');
+const path = require('path');
+
+describe('download', () => {
+  const destination = 'destination';
+  const relativeFilePath = '/some/file/path/file.zip'
+  const absoluteFilePath = 'file:///some/file/path/file.zip'
+  const remoteFilePath = 'http://website.com/files/file.zip'
+  const localManifestPath = '/some/manifest/path/manifest.json'
+  const remoteManifestPath = '/some/manifest/path/manifest.json'
+  const manifestJsonLocalFile = [{
+    "@id": absoluteFilePath,
+    "@type": "koio:KnowledgeObject"
+  }]
+  const manifestJsonRemoteFile = [{
+    "@id": remoteFilePath,
+    "@type": "koio:KnowledgeObject"
+  }]
+  let downloadOptions = {
+    extract: false,
+    headers: {accept: ['application/zip', 'application/octet-stream']}
+  };
+  const fs = td.replace('fs-extra');
+  const download = td.replace('download');
+  const admzip = td.replace('adm-zip');
+  const del = td.replace('del');
+
+  td.when(fs.existsSync(relativeFilePath)).thenResolve(() => {
+  })
+  td.when(fs.existsSync(localManifestPath)).thenResolve(() => {
+  })
+  td.when(fs.writeJsonSync(manifestJsonLocalFile)).thenResolve(() => {
+  })
+  td.when(fs.copySync(relativeFilePath, path.join(destination, path.basename(relativeFilePath)))).thenResolve(() => {
+  })
+  td.when(download(remoteFilePath, path.join(process.cwd()), downloadOptions)).thenResolve(null);
+  td.when(fs.readJsonSync(localManifestPath)).thenResolve(() => {
+    return manifestJsonLocalFile
+  })
+  td.when(fs.readJsonSync(remoteManifestPath)).thenResolve(() => {
+    return manifestJsonRemoteFile
+  })
+  td.when(del(td.matchers.anything())).thenResolve(() => {
+  });
+
+  describe('with files', () => {
+    test
+      .command([`download`, `-d`, `${destination}`, `-f`, `${relativeFilePath}`])
+      .it('should create the destination directory if not already there', (context) => {
+        td.verify(fs.ensureDirSync(destination))
+      });
+
+    test
+      .command([`download`, `-f`, `${relativeFilePath}`])
+      .it('should use the current directory if destination not set', (context) => {
+        td.verify(fs.ensureDirSync(process.cwd()))
+      });
+
+    test
+      .command([`download`, `-f`, `${relativeFilePath}`])
+      .it('should copy the relative file', (context) => {
+        td.verify(fs.copySync(relativeFilePath, path.join(process.cwd(), path.basename(relativeFilePath))))
+      });
+
+    test
+      .command([`download`, `-d`, `${destination}`, `-f`, `${relativeFilePath}`])
+      .it('should download the relative file to the destination', (context) => {
+        td.verify(fs.copySync(relativeFilePath, path.join(destination, path.basename(relativeFilePath))))
+      });
+
+    test
+      .command([`download`, `-f`, `${absoluteFilePath}`])
+      .it('should copy the absolute file', (context) => {
+        td.verify(fs.copySync(relativeFilePath, path.join(process.cwd(), path.basename(relativeFilePath))))
+      });
+
+    test
+      .command([`download`, `-f`, `${remoteFilePath}`])
+      .it('should download the remote file', (context) => {
+        td.verify(download(
+          remoteFilePath,
+          path.join(process.cwd()),
+          downloadOptions))
+      });
+
+    test
+      .command([`download`, `-f`, `${relativeFilePath}`])
+      .it('should write the relative file into the final manifest', (context) => {
+        td.verify(fs.writeJsonSync(path.join(process.cwd(), 'manifest.json'), manifestJsonLocalFile, {spaces: 4}))
+      });
+
+    test
+      .command([`download`, `-d`, `${destination}`, `-f`, `${relativeFilePath}`])
+      .it('should write the final manifest in the destination', (context) => {
+        td.verify(fs.writeJsonSync(path.join(destination, 'manifest.json'), manifestJsonLocalFile, {spaces: 4}))
+      });
+
+    test
+      .command([`download`, `-f`, `${absoluteFilePath}`])
+      .it('should write the absolute file into the final manifest', (context) => {
+        td.verify(fs.writeJsonSync(path.join(process.cwd(), 'manifest.json'), manifestJsonLocalFile, {spaces: 4}))
+      });
+
+    test
+      .command([`download`, `-f`, `${remoteFilePath}`])
+      .it('should write the remote file into the final manifest', (context) => {
+        td.verify(fs.writeJsonSync(path.join(process.cwd(), 'manifest.json'), manifestJsonRemoteFile, {spaces: 4}))
+      });
+
+    test
+      .command([`download`, `-d`, `${destination}`,`-f`, `${remoteFilePath}`])
+      .it('should delete the temporary directory', (context) => {
+        td.verify(del([path.join(destination, 'tmp')]))
+      });
+  });
+  describe('with manifest', () => {
+
+    test
+      .command([`download`, `-m`, `${localManifestPath}`])
+      .it('should read the local manifest json', (context) => {
+        td.verify(fs.readJsonSync(localManifestPath))
+      });
+
+    test
+      .command([`download`, `-m`, `${localManifestPath}`])
+      .it('should copy the local file in the manifest', (context) => {
+        td.verify(fs.copySync(relativeFilePath, path.join(process.cwd(), path.basename(relativeFilePath))))
+      });
+
+    test
+      .command([`download`, `-m`, `${remoteManifestPath}`])
+      .it('should read the remote manifest json', (context) => {
+        td.verify(fs.readJsonSync(remoteManifestPath))
+      });
+
+    test
+      .command([`download`, `-m`, `${remoteManifestPath}`])
+      .it('should download the remote file from the manifest', (context) => {
+        td.verify(download(
+          remoteFilePath,
+          path.join(process.cwd()),
+          downloadOptions))
+      });
+
+    test
+      .command([`download`, `-m`, `${localManifestPath}`])
+      .it('should write the local file from the original manifest into the final manifest', (context) => {
+        td.verify(fs.writeJsonSync(path.join(process.cwd(), 'manifest.json'), manifestJsonLocalFile, {spaces: 4}))
+      });
+
+    test
+      .command([`download`, `-m`, `${remoteManifestPath}`])
+      .it('should write the remote file from the original manifest into the final manifest', (context) => {
+        td.verify(fs.writeJsonSync(path.join(process.cwd(), 'manifest.json'), manifestJsonRemoteFile, {spaces: 4}))
+      });
+
+    test
+      .command([`download`, `-d`, `${destination}`,`-m`, `${remoteManifestPath}`])
+      .it('should delete the temporary directory', (context) => {
+        td.verify(del([path.join(destination, 'tmp')]))
+      });
+  });
+});
+

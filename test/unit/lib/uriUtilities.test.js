e62f96dead91d80de277b062556f8f480ded970c
mradamgbeck <mradamgbeck@gmail.com>
2021-10-06 13:39:17 -0400

    added uri Utilities

--- test/unit/lib/uriUtilities.test.js
+++ test/unit/lib/uriUtilities.test.js
@@ -1,7 +1,7 @@
 const {expect} = require('chai');
 const URI = require('uri-js');
 
-const {createUri} = require('../../../lib/uriUtilities')
+const {createUri} = require('../../../lib/uriUtilities');
 
 describe('uri Utilities', ()=>{
   describe('create Uri', ()=>{
@@ -9,23 +9,23 @@ describe('uri Utilities', ()=>{
       let inputUri = `file://${process.cwd()}/file.zip`;
       let expectedUri = URI.parse(inputUri);
       let output = createUri(inputUri);
-      expect(output).to.deep.equal(expectedUri)
-    })
+      expect(output).to.deep.equal(expectedUri);
+    });
 
     it('should create an absolute uri from an absolute remote uri', ()=>{
       let inputUri = `http://example.gov/directory/file.zip`;
       let expectedUri = URI.parse(inputUri);
       let output = createUri(inputUri);
-      expect(output).to.deep.equal(expectedUri)
-    })
+      expect(output).to.deep.equal(expectedUri);
+    });
 
-    it('should create an absolute uri from an relative local path and base in unix', ()=>{
-      let scheme = 'file'
-      let base = process.cwd()
+    it('should create an absolute uri from an relative local path and base', ()=>{
+      let scheme = 'file';
+      let base = process.cwd();
       let path = `file.zip`;
       let expectedUri = URI.parse(`${scheme}://${base}/${path}`);
       let uri = createUri(path, base);
-      expect(uri).to.deep.equal(expectedUri)
-    })
-  })
+      expect(uri).to.deep.equal(expectedUri);
+    });
+  });
 });
